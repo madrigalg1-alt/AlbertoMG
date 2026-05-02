96102e19f184beb66fa8201f28b47f7507861001
Alberto <madrigalg1@gmail.com>
2026-05-02 15:14:01 +0200

    Primera subida

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Introduzca el número desde donde quiere iniciar la cuenta atrás");
+            int superior = sc.nextInt();
+            System.out.println("Intruduzca el número donde quiere que se detenga la cuenta atrásssss");
+            int bajo = sc.nextInt();
+            for (int i = superior; i >=bajo ; i--) {
+                System.out.println(i);
+
+            }
+        }
+
+    }
